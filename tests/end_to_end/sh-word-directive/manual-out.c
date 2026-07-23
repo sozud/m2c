71e994b8dd6c17fc1d6cad5a4fe0bcb3c0e67090
sozud <122322823+sozud@users.noreply.github.com>
2026-07-23 20:38:24 +0700

    test: cover SH .word directive size

---
+++ tests/end_to_end/sh-word-directive/manual-out.c
@@ -0,0 +1,5 @@
+? literal();                                        /* static */
+
+s32 test(void) {
+    return 0x4000;
+}
---
+++ tests/end_to_end/sh-word-directive/manual.s
@@ -0,0 +1,6 @@
+glabel test
+mov.w literal, r0
+rts
+nop
+literal:
+.word 0x4000
